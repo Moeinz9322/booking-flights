44f698840fba81de2abbc95cbd45d8d84aade219
Moein_zanjirian <Moeinz9322@gmail.com>
2023-04-11 22:22:33 +0330

    add Flight , Time and Date classes

---
+++ src/Flight.java
@@ -0,0 +1,88 @@
+import java.sql.Time;
+import java.util.Date;
+
+public class Flight {
+    private String flightId;
+    private String origin;
+    private String destination;
+    private Date date;
+    private Time time;
+    private int price;
+    private int seats;
+    private int capacity;
+
+    public Flight(String flightId, String origin, String destination, Date date, Time time, int price, int seats, int capacity) {
+        this.flightId = flightId;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
+        this.time = time;
+        this.price = price;
+        this.seats = seats;
+        this.capacity = capacity;
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
